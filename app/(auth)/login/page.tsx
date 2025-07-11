8678de89ca08ea519c5db742da9c88692bc46081
Radu <radu.avadanii@gmail.com>
2025-07-11 10:38:16 +0300

    upd: login with github.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useTransition } from "react";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -8,9 +12,29 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader } from "lucide-react";
 
 export default function LoginPage() {
+    const [githubPending, startGithubTransition] = useTransition();
+
+    async function signInWithGithub() {
+        startGithubTransition(async () => {
+            await authClient.signIn.social({
+                provider: "github",
+                callbackURL: "/",
+                fetchOptions: {
+                    onSuccess: () => {
+                        toast.success("Successfully logged in with Github!");
+                    },
+                    onError: () => {
+                        toast.error(`Internal server error.`);
+                    },
+                },
+            });
+        });
+    }
+
     return (
         <Card className="">
             <CardHeader className="">
@@ -20,9 +44,23 @@ export default function LoginPage() {
                 </CardDescription>
             </CardHeader>
             <CardContent className="flex flex-col gap-4">
-                <Button className="w-full" variant="outline">
-                    <GithubIcon className="size-4" />
-                    Sign In with Github
+                <Button
+                    disabled={githubPending}
+                    onClick={signInWithGithub}
+                    className="w-full"
+                    variant="outline"
+                >
+                    {githubPending ? (
+                        <>
+                            <Loader className="size-4 animate-spin" />
+                            <span>Loading...</span>
+                        </>
+                    ) : (
+                        <>
+                            <GithubIcon className="size-4" />
+                            Sign In with Github
+                        </>
+                    )}
                 </Button>
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-card px-2 text-muted-foreground">
