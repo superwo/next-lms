4745af47c757e1e15d199bd774f924ee00de14d8
Radu <radu.avadanii@gmail.com>
2025-07-14 15:10:01 +0300

    init: dashboard.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import { buttonVariants } from "@/components/ui/button";
-import { toast } from "sonner";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -42,19 +37,6 @@ const features: FeatureProps[] = [
 ];
 
 export default function Home() {
-    const router = useRouter();
-
-    async function signOut() {
-        await authClient.signOut({
-            fetchOptions: {
-                onSuccess: () => {
-                    router.push("/");
-                    toast.success("Successfully logged out!");
-                },
-            },
-        });
-    }
-
     return (
         <>
             <section className="relative py-20">
@@ -90,7 +72,7 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-            <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-32">
                 {features.map((feature, index) => (
                     <Card
                         key={index}
